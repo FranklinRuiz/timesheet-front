3cbf5955050f1921d2904d63ea2708992787f398
FranklinRuiz <franklin.asto.leon@gmail.com>
2022-05-20 00:10:41 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiauth: 'http://147.182.215.248:8088',
-    apiurl: 'http://147.182.215.248:8080'
+    apiurl: 'http://147.182.215.248:8081'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiauth:'http://147.182.215.248:8088',
-    apiurl:'http://localhost:8080'
+    apiurl:'http://localhost:8081'
 };
